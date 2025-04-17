a8e451e2a36daec87006603310f6f12b5fb34eab
essyingram <ingrame@carleton.edu>
2025-04-17 14:32:05 -0400

    added HP character, potion, and spell datasets. updated description of cli.py

--- individual/cli.py
+++ individual/cli.py
@@ -2,5 +2,13 @@
 	cli.py
 	Essy Ingram, 17 April 2025
 	
-	practice with creating a usable command-line interface
+	NAME: cli.py - command-line interface exercise
+	SYNOPSIS: python3 cli.py booksby authorname
+	DESCRIPTION: Shows a list of the titles of all the books written by any
+	author whose name contains the specified authorname string, case-insensitively.
 '''
+
+import sys
+
+for index, argument in enumerate(sys.argv):
+    print(f'{index}: /{argument}/')
