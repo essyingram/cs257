51f91ca9273031af654b17710060d9dc2550ec20
essyingram <ingrame@carleton.edu>
2025-04-17 17:43:46 -0400

    cli.py features complete! added functionality to search characters by hogwarts house

--- individual/cli.py
+++ individual/cli.py
@@ -1,14 +1,36 @@
 '''
-	cli.py
-	Essy Ingram, 17 April 2025
-	
-	NAME: cli.py - command-line interface exercise
-	SYNOPSIS: python3 cli.py booksby authorname
-	DESCRIPTION: Shows a list of the titles of all the books written by any
-	author whose name contains the specified authorname string, case-insensitively.
+cli.py
+Essy Ingram, 17 April 2025
+
+NAME: cli.py - command-line interface exercise
+SYNOPSIS: python3 cli.py hogwartshouse
+DESCRIPTION: Shows a list of the names of all of the people belonging
+to the specified hogwartshouse string from the Harry Potter series, case-insensitively.
 '''
 
-import sys
+import csv
+import argparse
 
-for index, argument in enumerate(sys.argv):
-    print(f'{index}: /{argument}/')
+def get_parsed_arguments():
+    parser = argparse.ArgumentParser(description='Lists the Harry Potter characters who belong to a user-specified Hogwarts House.')
+    parser.add_argument('house', help='A Hogwarts house whose members you wish to identify')
+    return parser.parse_args()
+
+def main():
+    arguments = get_parsed_arguments()
+    house = arguments.house.lower()
+    houses = {'gryffindor', 'hufflepuff', 'ravenclaw', 'slytherin'}
+    
+    if house in houses:
+        with open('../data/characters.csv') as f:
+            reader = csv.reader(f, delimiter=';')
+            for character_row in reader:
+                actual_house = character_row[4].lower()
+                if house == actual_house:
+                    name = character_row[1]
+                    print(name)
+    else:
+        print("The house you have entered does not exist in the Harry Potter universe.")
+
+if __name__ == '__main__':
+    main()
